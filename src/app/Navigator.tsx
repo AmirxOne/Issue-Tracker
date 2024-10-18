deaaff315717305200c2c1e4d048dfe0da9cbed4
Axo <sahar.khiz.1379@gmail.com>
2024-10-18 15:16:50 +0330

    create Navigator

--- src/app/Navigator.tsx
+++ src/app/Navigator.tsx
@@ -1,9 +1,40 @@
+"use client"
 import React from 'react'
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import classnames from 'classnames';
+import { AiFillBug } from "react-icons/ai";
+
+const data = [
+    { id: 1, lable: "Dashboard", href: "/" },
+    { id: 2, lable: "issues", href: "/issues" },
+]
 
 function Navigator() {
-  return (
-    <div>Navigator</div>
-  )
+    const currentPathName = usePathname();
+
+    return (
+        <nav className='flex items-center space-x-6 px-5 h-14 border-b'>
+            <Link href="/"><AiFillBug /></Link>
+            <ul className='flex space-x-6'>
+                {
+                    data.map(item => (
+                        <Link
+                            key={item.id}
+                            href={item.href}
+                            className={classnames({
+                                'text-zinc-900': item.href === currentPathName,
+                                'text-zinc-500': item.href !== currentPathName,
+                                'hover:text-zinc-800 transition-colors duration-300': true
+                            })}
+                        >
+                            {item.lable}
+                        </Link>
+                    ))
+                }
+            </ul>
+        </nav>
+    )
 }
 
 export default Navigator
